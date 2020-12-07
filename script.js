11450dbf6d5794588dfcafff071b53cc84d3f4d4
Erik Andreas Pørksen <201404588@post.au.dk>
2020-12-07 23:32:16 +0100

    comment

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ DataSet.setDaysOfWeek([5]);
 DataSet.setDateRange({"startDate": new Date("2018-01-01"), "endDate": new Date("2018-08-31")});
 DataSet.setGeoAreaFilter({"geoArea": ALL_AIRPORTS, "checkOrigin": true, "checkDestination": true});
 
-let dataset_1 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2018.csv", "https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv"); 
+let dataset_1 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2018.csv", "https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv");
 let dataset_2 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2019.csv", "https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv"); //"https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv"
 let dataset_3 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2020.csv", "https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv");//"https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv"); //"Data/US_Covid.csv");
 
@@ -19,3 +19,7 @@ let dateBrush_2 = new DateBrush(2019, false);
 let dateBrush_3 = new DateBrush(2020, true);
 DateBrush.brushesList =  [dateBrush_1, dateBrush_2, dateBrush_3];
 DateBrush.lastDictator = dateBrush_3;
+
+
+//NOTE: Look at 26 Jun - 10 Jul, noticable differences (if looking at specific days of week, make sure length is of period is appropriate)
+// for example, looking only at fridays, period should have the same amount of each day in it, but since different dates fall on different days across the 3 years picking numbers divisible by 7 is highly advised
