5d37b2a7d6f186137cf0336ffe192e1ff7070a68
unknown <nordstroem92@gmail.com>
2020-12-07 12:15:39 +0100

    lokale filepaths

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ DataSet.setDaysOfWeek([5]);
 DataSet.setDateRange({"startDate": new Date("2018-01-01"), "endDate": new Date("2018-08-31")});
 DataSet.setGeoAreaFilter({"geoArea": ALL_AIRPORTS, "checkOrigin": true, "checkDestination": true});
 
-let dataset_1 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2018.csv", "https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv"); 
-let dataset_2 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2019.csv", "https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv"); //"Data/US_Covid.csv"
-let dataset_3 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2020.csv", "https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv");//"https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv"); //"Data/US_Covid.csv");
+let dataset_1 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2018.csv", "Data/US_Covid.csv"); 
+let dataset_2 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2019.csv", "Data/US_Covid.csv"); //"Data/US_Covid.csv"
+let dataset_3 = new DataSet("https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/flights_2020.csv", "Data/US_Covid.csv");//"https://raw.githubusercontent.com/nordstroem92/datavisualisering/master/Data/US_Covid.csv"); //"Data/US_Covid.csv");
 
 dataset_1.refresh().then(data => visualization1 = new DaVi("#svg_flights_2018", data));
 dataset_2.refresh().then(data => visualization2 = new DaVi("#svg_flights_2019", data));
